06ca9ccb2be132fb0e321fe795800dc08225ac5d
KautsarH <kautsarhasanah@gmail.com>
2021-01-23 23:41:07 +0800

    test radius

--- app/Http/Controllers/PlannerController.php
+++ app/Http/Controllers/PlannerController.php
@@ -49,7 +49,7 @@ class PlannerController extends Controller
         //$stations = \App\Station::orderBy('id')->get(); //starts w 0
         $stations = \App\Station::where('status', 'active')->orderBy('id')->get();
         $fstation = \App\Station::where('status', 'active')->orderBy('id')->first()->id;
-        $radius = 130;
+        $radius = 100;
         $locations = new Collection;
 
         $place = $request['place1'];
