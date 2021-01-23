a8b5b6492e6022f5923eceb91edd709c8f76031d
KautsarH <kautsarhasanah@gmail.com>
2021-01-23 22:28:38 +0800

    test algo

--- app/Http/Controllers/PlannerController.php
+++ app/Http/Controllers/PlannerController.php
@@ -46,8 +46,8 @@ class PlannerController extends Controller
         $numstation = count(\App\Station::orderBy('id')->get());
         $inactivedb = \App\Station::where('status', 'inactive')->orderBy('id')->pluck('id');
         $inactive = (array)$inactivedb;
-        $stations = \App\Station::orderBy('id')->get(); //starts w 0
-        // $stations = \App\Station::where('status', 'active')->orderBy('id')->get();
+        //$stations = \App\Station::orderBy('id')->get(); //starts w 0
+        $stations = \App\Station::where('status', 'active')->orderBy('id')->get();
         $fstation = \App\Station::where('status', 'active')->orderBy('id')->first()->id;
         $radius = 150;
         $locations = new Collection;
