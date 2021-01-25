25631abaeabd64bbaff02ce8739a6c9da098e6ed
KautsarH <kautsarhasanah@gmail.com>
2021-01-26 01:47:43 +0800

    test algo

--- app/Http/Controllers/PlannerController.php
+++ app/Http/Controllers/PlannerController.php
@@ -85,7 +85,7 @@ class PlannerController extends Controller
                     $lat = $stations[$i]->lat;
                     $lng = $stations[$i]->lng;
                     $location = $lat. "," .$lng;
-                    dd($location);
+                    //dd($location);
 
                     //list of places nearby the station
                     $response = GooglePlaces::nearbySearch($location, $radius,$keyword);
